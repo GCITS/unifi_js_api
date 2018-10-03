8f0e6b1c5d4620ba0f0c1aa66e111e724f0b05e1
Dion Goile <dion@gcits.com>
2018-10-03 16:32:40 +1000

    Working queries

--- index.js
+++ index.js
@@ -1,18 +1,62 @@
 const MongoClient = require("mongodb").MongoClient;
 const assert = require("assert");
 
-const url = "mongodb://localhost:27117";
+// Connection URL
+const url = "mongodb://localhost:27017";
 
-const dbName = "ace";
+// Database Name
+const dbName = "unifi-test";
 
-MongoClient.connect(
-  url,
-  function(err, client) {
-    assert.equal(null, err);
-    console.log("Connected successfully to server");
+const getDevices = function(db, callback, site) {
+  // Get the documents collection
+  const collection = db.collection("device");
+  // Find some documents
+  collection.find({ site_id: site }).toArray(function(err, docs) {
+    assert.equal(err, null);
 
-    const db = client.db(dbName);
+    const result = [];
+    docs.forEach(element => {
+      result.push({
+        DeviceType: element.type,
+        DeviceName: element.name
+      });
+    });
+    callback(result);
+    console.log("Found the following records");
+    console.log(result);
+  });
+};
+const getPortforward = function(db, callback, site) {
+  // Get the documents collection
+  const collection = db.collection("portforward");
+  // Find some documents
+  collection.find({ site_id: site }).toArray(function(err, docs) {
+    assert.equal(err, null);
+    console.log("Found the following records");
+    console.log(docs);
+    callback(docs);
+  });
+};
 
-    client.close;
-  }
-);
+function callMongo(actor, database, site) {
+  MongoClient.connect(
+    url,
+    function(err, client) {
+      assert.equal(null, err);
+      console.log("Connected correctly to server");
+
+      const db = client.db(database);
+
+      actor(
+        db,
+        function() {
+          client.close();
+        },
+        site
+      );
+    }
+  );
+}
+// Use connect method to connect to the server
+callMongo(getDevices, dbName, "58ddab39e4b00eb5796c2533");
+callMongo(getPortforward, dbName, "59cb0368e4b0fb3f5ada7440");
